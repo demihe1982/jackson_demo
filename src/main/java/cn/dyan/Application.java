344fada7849e7b8555aa6e6453f7bdf717b2cd39
edayahe <demi.he@ericsson.com>
2017-09-27 17:35:25 +0800

    mybatis+jdbc transaction

--- src/main/java/cn/dyan/Application.java
+++ src/main/java/cn/dyan/Application.java
@@ -6,14 +6,23 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 
+import java.sql.SQLException;
+
 @SpringBootApplication
 @MapperScan(basePackages = {"cn.dyan.mybatis.mapper"})
 public class Application {
     public static void main(String[] args){
         ApplicationContext context =  SpringApplication.run(Application.class,args);
         DemoService service = context.getBean(DemoService.class);
-        service.queryAll().stream().forEach(tbUser -> {
-            System.out.println("uid:"+tbUser.getUuid()+" uname:"+tbUser.getUname());
-        });
+
+        try {
+            service.createUser("test123");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+//        service.queryAll().stream().forEach(tbUser -> {
+//            System.out.println("uid:"+tbUser.getUuid()+" uname:"+tbUser.getUname());
+//        });
+
     }
 }
--- src/main/java/cn/dyan/services/DemoService.java
+++ src/main/java/cn/dyan/services/DemoService.java
@@ -3,13 +3,21 @@ package cn.dyan.services;
 import cn.dyan.model.DemoVo;
 import cn.dyan.mybatis.domain.TbUser;
 import cn.dyan.mybatis.mapper.TbUserMapper;
+import com.mysql.jdbc.exceptions.jdbc4.MySQLSyntaxErrorException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.sql.DataSource;
 import java.beans.DesignMode;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
+import java.util.UUID;
 
 @Component
 public class DemoService {
@@ -18,6 +26,9 @@ public class DemoService {
 
     @Autowired
     private TbUserMapper userMapper;
+
+    @Autowired
+    private DataSource dataSource;
 
     public void test(){
         DemoVo vo = new DemoVo();
@@ -30,5 +41,24 @@ public class DemoService {
 
     public List<TbUser> queryAll(){
         return userMapper.selectAll();
+    }
+
+    @Transactional(rollbackFor = MySQLSyntaxErrorException.class)
+    public boolean createUser(String uname) throws SQLException {
+        TbUser user = new TbUser();
+        user.setUname(uname);
+        user.setUuid(UUID.randomUUID().toString());
+        userMapper.insert(user);
+
+        Connection cnn = DataSourceUtils.getConnection(dataSource);
+        Statement stmt = null;
+        try{
+            stmt = cnn.createStatement();
+            stmt.execute("INSERT INTO tb_test(uuid,test) VALUES('1111','test1111')");
+        }finally {
+            DataSourceUtils.releaseConnection(cnn,dataSource);
+        }
+        return true;
+
     }
 }
